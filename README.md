c707b8b9c580302a0e63abbe0f3e0e3e1de15878
wen <wenwangbusy@gmail.com>
2016-11-09 11:53:48 -0800

    action values

--- README.md
+++ README.md
@@ -13,4 +13,21 @@ About this environment:
 * An action is a 17-dimensional vector. So the robot has 17 DOF. 
 * An obsevation is 376-dimensional vector. They probably contains positions, speed, acceleration, angular velocities in the joins. It is not interesting to look into these, although Mujoco has some papers (such as [this one](http://homes.cs.washington.edu/~todorov/papers/TassaIROS12.pdf)) to track. It is more interesting to develop an algorithm without knowing what these states are. 
 
+Range of the action values:
+
+```
+env = gym.make('HumanoidStandup-v1')
+a_samples = []
+for i in range(100):
+    action = env.action_space.sample()
+    a_samples.append(action)
+mins = np.min(a_samples, 0)
+maxes = np.max(a_samples, 0)
+print(mins)
+print(maxes)
+
+plt.plot(mins)
+plt.plot(maxes)
+plt.show()
+```
 
--- test.py
+++ test.py
@@ -1,22 +1,34 @@
 import gym
-import sys
-
-print(sys.executable)
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 env = gym.make('HumanoidStandup-v1')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        action = env.action_space.sample()
+a_samples = []
+for i in range(100):
+    action = env.action_space.sample()
+    a_samples.append(action)
+mins = np.min(a_samples, 0)
+maxes = np.max(a_samples, 0)
+print(mins)
+print(maxes)
 
-        if t == 0:
-            print(observation.shape)
-            print(observation)
-            print(action.shape)
+plt.plot(mins)
+plt.plot(maxes)
+plt.show()
 
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+
+# for i_episode in range(20):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         action = env.action_space.sample()
+#
+#         if t == 0:
+#             print(observation.shape)
+#             print(observation)
+#             print(action.shape)
+#
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
